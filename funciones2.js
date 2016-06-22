e648414069687b3560f2c9686d9dffa83fe727e1
Daynamon <dsmonsalve83@gmail.com>
2016-06-22 16:29:31 -0500

    Update funciones2.js

--- funciones2.js
+++ funciones2.js
@@ -1,18 +1,19 @@
 var x;
 x=$(document);
 x.ready(inicializarEventos);
-x.ready(presionH1);
+
 
 function inicializarEventos()
 {
   var x;
   x=$("h1");
+  x.ready(presionH1)
 }
 
 
 function presionH1()
 {
   var x;
-  x=$(this);
-  x.css("color","#fff");
+  x=$("#encabezado h1");
+  x.css("color","#ffffff");
 }
